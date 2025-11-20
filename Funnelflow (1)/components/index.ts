4e36ad656adf3daa0b1c31ed76fdf04addf8f0ef
grace-and-truth <admin@graceandtruth.website>
2025-11-20 22:42:57 +0300

    Create index.ts to re-export core components

---
+++ Funnelflow (1)/components/index.ts
@@ -0,0 +1,10 @@
+// Re-export core components
+export { default as Card } from './Card';
+export { default as ContextMenu } from './ContextMenu';
+export { default as DropDownMenu } from './DropDownMenu';
+export { default as Header } from './Header';
+export { default as Modal } from './Modal';
+export { default as Sidebar } from './Sidebar';
+
+// Re-export everything from Icons folder
+export * from './Icons';
