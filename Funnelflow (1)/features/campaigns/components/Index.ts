8e8abaf0d8ba984467f580e7ccaf1856141eb136
grace-and-truth <admin@graceandtruth.website>
2025-11-28 22:08:39 +0300

    Create Index.ts to export campaign components

---
+++ Funnelflow (1)/features/campaigns/components/Index.ts
@@ -0,0 +1,9 @@
+export { default as AssetPickerModal } from './AssetPickerModal';
+export { default as BlockPicker } from './BlockPicker';
+export { default as BlockWrapper } from './BlockWrapper';
+export { default as InlinePlaceholder } from './InlinePlaceholder';
+export { default as LinkEditorModal } from './LinkEditorModal';
+export { default as LinkInput } from './linkInput';
+export { default as StylePanel } from './StylePanel';
+export { default as VisualEmailEditor } from './VisualEmailEditor';
+export * from './renderers';
