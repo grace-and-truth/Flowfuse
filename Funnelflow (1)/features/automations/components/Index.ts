fc416a5c6ef145179f1701ea9b29e0569386ce23
grace-and-truth <admin@graceandtruth.website>
2025-11-28 22:24:17 +0300

    Create Index.ts to export automation components

---
+++ Funnelflow (1)/features/automations/components/Index.ts
@@ -0,0 +1,4 @@
+export { default as AutomationAnnetation } from './AutomationAnnotation';
+export { default as AutomationNode } from './AutomationNode';
+export { default as NodeConfigPanel } from './NodeConfigPanel';
+export { default as NodeSidebar } from './NodeSidebar';
