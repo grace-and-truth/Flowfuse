093a55d54e38cc9a29066a366d8d6817e90edefa
grace-and-truth <admin@graceandtruth.website>
2025-11-28 22:28:52 +0300

    Export automation-related components and views

---
+++ Funnelflow (1)/features/automations/Index.ts
@@ -0,0 +1,4 @@
+export { default as AutomationEditor } from './AutomationEditor';
+export { default as AutomationsView } from './AutomationsView';
+export { default as RenameAutomationModal } from './RenameAutomationModal';
+export * from './components';
