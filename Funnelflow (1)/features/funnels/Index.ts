a3b27235daa93b162984ba406451df2e3d040326
grace-and-truth <admin@graceandtruth.website>
2025-11-28 21:38:13 +0300

    Create Index.ts to export funnel components

---
+++ Funnelflow (1)/features/funnels/Index.ts
@@ -0,0 +1,6 @@
+export { default as FunnelStepModal } from './FunnelStepModal';
+export { default as EditFunnelStepModal } from './EditFunnelStepModal';
+export { default as FunnelsView } from './FunnelsView';
+export { default as PreviewStepModal } from './PreviewStepModal';
+export { default as RenameFunnelModal } from './RenameFunnelModal';
+export { default as Templates } from './templates';
