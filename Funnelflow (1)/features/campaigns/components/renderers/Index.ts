6b5cf9b338eabc7642dba6967d169080fc4071ca
grace-and-truth <admin@graceandtruth.website>
2025-11-28 21:48:48 +0300

    Create Index.ts to export block components

---
+++ Funnelflow (1)/features/campaigns/components/renderers/Index.ts
@@ -0,0 +1,8 @@
+export { default as ButtonBlock } from './ButtonBlock';
+export { default as DividerBlock } from './DividerBlock';
+export { default as FormBlock } from './FormBlock';
+export { default as HeaderBlock } from './HeaderBlock';
+export { default as ImageBlock } from './ImageBlock';
+export { default as ShapeBlock } from './ShapeBlock';
+export { default as SpacerBlock } from './SpacerBlock';
+export { default as TextBlock } from './TextBlock';
